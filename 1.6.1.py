dd262e2adbe7d1c88274e603cd678155145f0884
Iliya <iliyaboyarov317@gmail.com>
2022-03-08 19:00:20 +0300

    exercise 1.6 stepik

---
+++ 1.6.1.py
@@ -0,0 +1,34 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.keys import Keys
+import pandas as pd
+import numpy as np
+import re
+import datetime as dt
+from selenium.webdriver.common.by import By
+import time
+
+
+service = ChromeService(executable_path='C:\\Users\\Илья\\.wdm\\drivers\\chromedriver\\win32\\97.0.4692.71\\chromedriver.exe')
+browser = webdriver.Chrome(service=service)
+link = "http://suninjuly.github.io/simple_form_find_task.html"
+
+try:
+    browser.get(link)
+    input1 = browser.find_element(By.TAG_NAME, "input")
+    input1.send_keys("Ivan")
+    input2 = browser.find_element(By.NAME, "last_name")
+    input2.send_keys("Petrov")
+    input3 = browser.find_element(By.CLASS_NAME, "form-control.city")
+    input3.send_keys("Smolensk")
+    input4 = browser.find_element(By.ID, "country")
+    input4.send_keys("Russia")
+    button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+    button.click()
+
+finally:
+    # закрываем браузер после всех манипуляций
+
+    time.sleep(30)
+    browser.quit()
