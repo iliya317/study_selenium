8b66156a12f83e98690d6cf269515b432bd83bd8
Iliya <iliyaboyarov317@gmail.com>
2022-03-09 21:17:31 +0300

    exercise 1.6.4 stepik

--- 1.6.4.py
+++ 1.6.4.py
@@ -8,11 +8,10 @@ import re
 import datetime as dt
 from selenium.webdriver.common.by import By
 import time
-
-
+import math
 service = ChromeService(executable_path='C:\\Users\\Илья\\.wdm\\drivers\\chromedriver\\win32\\97.0.4692.71\\chromedriver.exe')
 browser = webdriver.Chrome(service=service)
-link = "http://suninjuly.github.io/simple_form_find_task.html"
+link = "http://suninjuly.github.io/find_xpath_form"
 
 try:
     browser.get(link)
@@ -24,8 +23,8 @@ try:
     input3.send_keys("Smolensk")
     input4 = browser.find_element(By.ID, "country")
     input4.send_keys("Russia")
-    button = browser.find_element(By.CSS_SELECTOR, "button.btn")
-    button.click()
+    element = browser.find_element(By.XPATH, "/html/body/div[1]/form/div[6]/button[3]")
+    element.click()
 
 finally:
     # закрываем браузер после всех манипуляций
